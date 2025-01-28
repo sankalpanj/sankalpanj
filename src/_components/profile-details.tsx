df377c920a8b6fddd6e72248d992769d71004305
mayurkarmakar07 <mayurkarmakar07@gmail.com>
2025-01-28 08:25:51 +0530

    Fixed typo in profile-details file

--- src/_components/profile-details.tsx
+++ src/_components/profile-details.tsx
@@ -66,7 +66,7 @@ function RenderUserProfileSettings({ userProfile }: Props) {
             <CardHeader>
               <CardTitle>Password</CardTitle>
               <CardDescription>
-                Change your password here. After saving, you'll be logged out.
+                Change your password here. After saving, you&apos;ll be logged out.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
